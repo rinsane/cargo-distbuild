9a67dbf89e63dae16b16d9e2ac455a207bfabcdd
rinsane <realrishabh9@gmail.com>
2025-12-02 16:53:30 +0530

    BUG: wrapper env not being set fix

--- src/wrapper_main.rs
+++ src/wrapper_main.rs
@@ -1,11 +1,9 @@
 // Wrapper binary entry point
 // This is what Cargo will call instead of rustc
 
-use cargo_distbuild::wrapper;
-
 #[tokio::main]
 async fn main() {
-    if let Err(e) = wrapper::run_wrapper().await {
+    if let Err(e) = cargo_distbuild::wrapper::run_wrapper().await {
         eprintln!("cargo-distbuild wrapper error: {}", e);
         std::process::exit(1);
     }
